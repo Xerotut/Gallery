405379cb434c7f7fd45a2e02575eeed4b8370f72
Xerotut <Xerotut@gmail.com>
2023-06-22 00:45:37 +0300

    Clean up after brunch creation
    Messed up brunch-donwloader-generic brunch creation a bit, cleaned up

--- Assets/Scripts/UI/GalleryImage.cs
+++ Assets/Scripts/UI/GalleryImage.cs
@@ -18,38 +18,38 @@ namespace Gallery
         private void Awake()
         {
             numberOfImages++;
-            string url = WebUtility.AssembleURL(new string[] {_url, numberOfImages.ToString(),".jpg" });
+            string url = AssembleURL(new string[] {_url, numberOfImages.ToString(),".jpg" });
             _image = GetComponent<Image>();
-            WebUtility.DownloadTexture2D(url, sprite => AssignSprite(sprite));
-            //StartCoroutine(GetTexture(url));
+            StartCoroutine(GetTexture(url));
             
         }
 
-
-        private void AssignSprite(Texture2D texture)
+        private IEnumerator GetTexture(string url)
         {
+            UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+            yield return request.SendWebRequest();
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(request.error);
+                _image.sprite = _errorImage;
+                yield break;
+            }
+
+            Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
             Sprite newSprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f));
             _image.sprite = newSprite;
+
         }
 
-        //private IEnumerator GetTexture(string url)
-        //{
-        //    UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
-        //    yield return request.SendWebRequest();
-        //    if (request.result != UnityWebRequest.Result.Success)
-        //    {
-        //        Debug.Log(request.error);
-        //        _image.sprite = _errorImage;
-        //        yield break;
-        //    }
-            
-        //    Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
-        //    Sprite newSprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f)); 
-        //    _image.sprite = newSprite;
-            
-        //}
-
-      
+        private string AssembleURL(string[] urlparts)
+        {
+            string url = "";
+            foreach (string part in urlparts)
+            {
+                url += part;
+            }
+            return url;
+        }
 
     }
 }
